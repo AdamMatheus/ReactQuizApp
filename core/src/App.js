0d6506f3dc1e040a01561ba3caab68df29ec140a
AdamMatheus <adam.matheus.it@gmail.com>
2022-04-09 16:48:04 +0200

    updated

---
+++ core/src/App.js
@@ -0,0 +1,22 @@
+
+import {Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import QuizSelect from './components/QuizSelect';
+import RandomQuiz from './components/RandomQuiz'
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<QuizSelect/>}/>
+        <Route path="/r/:topic" element={<RandomQuiz/>}/>
+
+      </Routes>
+
+    </Router>
+
+  );
+}
+
+export default App;
